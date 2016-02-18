4129ea2efd52117d2de53e3e3338438a031c8e5d
Are Edvardsen <are.edvardsen@helse-nord.no>
2016-02-18 13:46:33 +0100

    first commit tesing table...

---
+++ R/NGERtabXXI.R
@@ -0,0 +1,54 @@
+#' Generate tab XXI
+#'
+#' Yes, generate tab XXI
+#'
+#' @inheritParams FigAndeler
+#' @return dataframe tabXXI
+#' @export
+
+NGERtabXXI <- function(datoFra = '2014-01-01', datoTil = '2014-04-01') {
+
+  RegData <- NGERqueryXXI(datoFra, datoTil)
+  N <- dim(RegData)[1]
+  mydf <- RegData[ , c("ComplAfterBleed",
+                       "ComplAfterBleedAbdom",
+                       "ComplAfterBleedVaginal",
+                       "ComplAfterBleedIntra",
+                       "ComplEquipment",
+                       "ComplEquipNet",
+                       "ComplEquipInstruments",
+                       "ComplEquipSuture",
+                       "ComplInfection",
+                       "ComplInfSurg",
+                       "ComplInfIntra",
+                       "ComplInfEndoSalpin",
+                       "ComplInfUVI",
+                       "ComplInfOther",
+                       "ComplOrgan",
+                       "ComplOrganIntestinal",
+                       "ComplOrganBladder",
+                       "ComplOrganUreter",
+                       "ComplOrganKar",
+                       "ComplOrganOther",
+                       "ComplReop",
+                       "ComplReopLaparoscopy",
+                       "ComplReopHysteroscopy",
+                       "ComplReopLaparotomy"
+                       )
+                   ]
+  vars <- names(mydf)
+  Komplikasjon <- vector()
+  Frekvens <- vector()
+  Andel <- vector()
+  for (s in vars) {
+    Komplikasjon <- c(Komplikasjon, s)
+    Nk <- sum(mydf[ , s], na.rm = TRUE)
+    Frekvens <- c(Frekvens, Nk)
+    Andel <- Nk/N
+  }
+
+  tabXXI <- data.frame(Komplikasjon, Frekvens, Andel)
+
+  return(tabXXI)
+
+}
