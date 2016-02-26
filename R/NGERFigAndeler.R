05aaddde0139b7a440571669894012811473578b
kevinthon <kevin.thon@gmail.com>
2016-02-26 13:41:24 +0100

    Debug-crap fjernet

--- R/NGERFigAndeler.R
+++ R/NGERFigAndeler.R
@@ -65,11 +65,10 @@
 FigAndeler  <- function(RegData=0, valgtVar, datoFra='2013-01-01', datoTil='2050-12-31', minald=0, maxald=130,
                         outfile='', reshID, enhetsUtvalg=1, MCEType=99, hentData=0, preprosess=TRUE)
 {
-  print(hentData)
+
      ## Hvis spørring skjer fra R på server. ######################
      if(hentData == 1){
           RegData <- NGERHentRegData(datoFra = datoFra, datoTil = datoTil)
-          print(dim(RegData))
      }
 
      # Hvis RegData ikke har blitt preprosessert
