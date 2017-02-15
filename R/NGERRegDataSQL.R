ccc700b1c9a12654b85ee0c2a10253ad43ae23e5
Olsen Lena Ringstad <lro2402unn@hn.helsenord.no>
2017-02-15 15:24:41 +0100

    Oppdatert spørring ihht. ny struktur på data

--- R/NGERRegDataSQL.R
+++ R/NGERRegDataSQL.R
@@ -88,103 +88,106 @@ if (varUtvalg==0) {
 #  paste0('AlleVarNum.',varUtvalg,suffix=', \n'),
 query <- paste0('SELECT
   HysBlodning,
-  HysFluidOverload,
-  HysGjforingsGrad,
-  HysKomplikasjoner,
-  HysPerforasjon,
-  HysDiagnose1,
-  HysDiagnose2,
-  HysDiagnose3,
-  HysProsedyre1,
-  HysProsedyre2,
-  HysProsedyre3,
-  HysTeknisk,
-  HysTilgang,
-  LapAdherProfylakse,
-  LapBipolarDiatermi,
-  LapBlare,
-  LapClips,
-  LapHarmonicS,
-  LapHjelpeinnstikk,
-  LapIntKoagOgKlipp,
-  LapIntKombo,
-  LapIntraabdominell,
-  LapKarBlodning,
-  LapKomplikasjoner,
-  LapKompTilgang,
-  LapKonvertert,
-  LapMorcellatorMedPose,
-  LapMorcellatorUtenPose,
-  LapNerv,
-  LapNett,
-  LapNumHjelpeinnstikk,
-  LapDiagnose1,
-  LapDiagnose2,
-  LapDiagnose3,
-  LapProsedyre1,
-  LapProsedyre2,
-  LapProsedyre3,
-  LapPostoperativ,
-  LapPreparatopose,
-  LapRobotKirurgi,
-  LapSingelPort,
-  LapStaplerEndogia,
-  LapSutur,
-  LapTarm,
-  LapTekniskUtstyr,
-  LapTilgang,
-  LapTilgangsMetode,
-  LapUnipolarDiatermi,
-  LapUreter,
-  LapUterusmanipulator,
-  OpAnestesi,
-  OpAntibProfylakse,
-  OpASA,
-  OpBMI,
-  OpBMIKategori,
-  OpDagkirurgi,
-  OpDato,
-  OpIVaktTid,
-  OpKategori,
-  OpMetode,
-  OpTidlLaparotomi,
-  OpTidlLapsko,
-  OpTidlVagInngrep,
-  AlleVarNum.OpType,
-  AlleVarNum.SivilStatus,
-  AlleVarNum.Utdanning,
-  FollowupsNum.Opf0AlvorlighetsGrad
-  ,FollowupsNum.Opf0KomplBlodning
-  ,FollowupsNum.Opf0Komplikasjoner
-  ,FollowupsNum.Opf0KomplInfeksjon
-  ,FollowupsNum.Opf0KomplOrgan
-  ,FollowupsNum.Opf0KomplUtstyr
-  ,FollowupsNum.Opf0Reoperasjon
-  ,FollowupsNum.Opf0Status
-  ,ForlopsOversikt.AvdRESH
-  ,ForlopsOversikt.BasisRegStatus
-  ,ForlopsOversikt.Fodselsdato
-  ,ForlopsOversikt.HovedDato
-  ,ForlopsOversikt.Norsktalende
-  ,ForlopsOversikt.OppflgRegStatus
-  ,ForlopsOversikt.OppflgStatus
-  ,ForlopsOversikt.PasientID
-  ,ForlopsOversikt.PasientAlder
-  ,ForlopsOversikt.SykehusNavn
-  FROM AlleVarNum
-  INNER JOIN ForlopsOversikt
-  ON AlleVarNum.ForlopsID = ForlopsOversikt.ForlopsID
-  LEFT JOIN FollowupsNum
-  ON ForlopsOversikt.ForlopsID = FollowupsNum.ForlopsID
+    HysFluidOverload,
+    HysGjforingsGrad,
+    HysKomplikasjoner,
+    HysPerforasjon,
+    HysDiagnose1,
+    HysDiagnose2,
+    HysDiagnose3,
+    HysProsedyre1,
+    HysProsedyre2,
+    HysProsedyre3,
+    HysTeknisk,
+    HysTilgang,
+    LapAdherProfylakse,
+    LapBipolarDiatermi,
+    LapBlare,
+    LapClips,
+    LapHarmonicS,
+    LapHjelpeinnstikk,
+    LapIntKoagOgKlipp,
+    LapIntKombo,
+    LapIntraabdominell,
+    LapKarBlodning,
+    LapKomplikasjoner,
+    LapKompTilgang,
+    LapKonvertert,
+    LapMorcellatorMedPose,
+    LapMorcellatorUtenPose,
+    LapNerv,
+    LapNett,
+    LapNumHjelpeinnstikk,
+    LapDiagnose1,
+    LapDiagnose2,
+    LapDiagnose3,
+    LapProsedyre1,
+    LapProsedyre2,
+    LapProsedyre3,
+    LapPostoperativ,
+    LapPreparatopose,
+    LapRobotKirurgi,
+    LapSingelPort,
+    LapStaplerEndogia,
+    LapSutur,
+    LapTarm,
+    LapTekniskUtstyr,
+    LapTilgang,
+    LapTilgangsMetode,
+    LapUnipolarDiatermi,
+    LapUreter,
+    LapUterusmanipulator,
+    OpAnestesi,
+    OpAntibProfylakse,
+    OpASA,
+    OpBMI,
+    OpBMIKategori,
+    OpDagkirurgi,
+    OpDato,
+    OpIVaktTid,
+    OpKategori,
+    OpMetode,
+    OpTidlLaparotomi,
+    OpTidlLapsko,
+    OpTidlVagInngrep,
+    OpType,
+    AlleVarNum.SivilStatus,
+    Utdanning,
+    Opf0AlvorlighetsGrad,
+    Opf0KomplBlodning,
+    Opf0Komplikasjoner,
+    Opf0KomplInfeksjon,
+    Opf0KomplOrgan,
+    Opf0KomplUtstyr,
+    Opf0Reoperasjon,
+    Opf0Status,
+    AlleVarNum.AvdRESH,
+    AlleVarNum.Norsktalende,
+    AlleVarNum.PasientID
+    ,ForlopsOversikt.BasisRegStatus
+    ,ForlopsOversikt.FodselsDato AS Fodselsdato
+    ,ForlopsOversikt.HovedDato
+    ,ForlopsOversikt.OppflgRegStatus
+    ,ForlopsOversikt.OppflgStatus
+    ,ForlopsOversikt.PasientAlder
+    ,ForlopsOversikt.SykehusNavn
+    FROM AlleVarNum
+    INNER JOIN ForlopsOversikt
+    ON AlleVarNum.ForlopsID = ForlopsOversikt.ForlopsID
 WHERE HovedDato >= \'', datoFra, '\' AND HovedDato <= \'', datoTil, '\'')
+
+#  FROM AlleVarNum
+#  INNER JOIN ForlopsOversikt
+#  ON AlleVarNum.ForlopsID = ForlopsOversikt.ForlopsID
+#  LEFT JOIN FollowupsNum
+#  ON ForlopsOversikt.ForlopsID = FollowupsNum.ForlopsID
+# WHERE HovedDato >= \'', datoFra, '\' AND HovedDato <= \'', datoTil, '\'')
 
   #LapMorcellator,  Erstattet
   #LapPlasmajet, Fjernet
   #?    OpOptimeCount,
   #?    OpParities,
   #?    OpPregnancies,
-
-
 
 
 
