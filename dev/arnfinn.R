92b09dc477a04fe25c8f1afe01b6d9ff7e9a2134
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2026-01-21 14:54:14 +0100

    Månedsrapport i markdown-format
    Figurene må bli høyere.

--- dev/arnfinn.R
+++ dev/arnfinn.R
@@ -1,16 +1,19 @@
 
-#sship::dec("c://Users/ast046/Downloads/nger17bc480df.sql.gz__20250304_095058.tar.gz",
-#           keyfile = "p://.ssh/id_rsa",
+#sship::dec("c://Users/ast046/Downloads/nger15b2d454c.sql.gz__20260121_104135.tar.gz",
+#           keyfile = "c://Users/ast046/.ssh/id_rsa",
 #           target_dir = "c://Users/ast046/Downloads/."
 #           )
 
 
 Sys.setlocale(locale = 'nb_NO.UTF-8')
-source("dev/sysSetenv.R")
 Sys.setenv(R_RAP_INSTANCE="QAC")
 Sys.setenv(R_RAP_CONFIG_PATH="C:/Users/ast046/repo/rapporteket/rygg/dev/config")
 Sys.setenv(MYSQL_DB_DATA="nger")
 
 Sys.setenv(MYSQL_HOST="localhost") # for mobilt kontor
+
+source("dev/sysSetenv.R")
+rmarkdown::render("inst/NGERmndRapp.Rmd", params = list(reshId = 110734))
+
 nger::kjor_NGERapp(browser = TRUE)
 
